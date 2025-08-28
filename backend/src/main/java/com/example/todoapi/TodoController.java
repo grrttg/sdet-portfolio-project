170d1710278006a69a30ee97d7dc79c35bb79f7c
Garrett Gue <garrettgue@gmail.com>
2025-08-28 14:22:09 -0700

    feat: Build initial backend API for to-do items

---
+++ backend/src/main/java/com/example/todoapi/TodoController.java
@@ -0,0 +1,31 @@
+package com.example.todoapi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
+public class TodoController {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @GetMapping
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Todo createTodo(@RequestBody Todo todo) {
+        return todoRepository.save(todo);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable Long id) {
+        todoRepository.deleteById(id);
+    }
+}
